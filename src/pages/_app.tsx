b53be75b9a7466f0ee2ab21f5c8f05dff3e07b50
Neel <neelp0980@gmail.com>
2023-10-29 17:52:03 +0530

    feat: Hero view

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="min-h-[calc(100vh-68px)] pt-16 px-2 sm:px-4">
+      <Component {...pageProps} />
+    </div>
+  );
 }
