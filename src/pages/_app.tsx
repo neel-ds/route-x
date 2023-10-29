690dff5f719348e7b1b8810e7e75f0691572f14d
Neel <neelp0980@gmail.com>
2023-10-29 18:08:20 +0530

    feat: footer component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import NavBar from "@/components/NavBar";
 import { ChakraProvider } from "@chakra-ui/react";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </ChakraProvider>
       </div>
+      <Footer />
     </ThemeProvider>
   );
 }
